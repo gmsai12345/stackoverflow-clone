201e538dabcbff26679c70dcccfe162531eaa4ce
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2023-01-01 01:21:50 +0530

    Update index.js

--- client1/src/api/index.js
+++ client1/src/api/index.js
@@ -1,5 +1,7 @@
 // to retreive data from all parts of website
 import axios from "axios";
-const API = axios.create({baseURL:"http://localhost:5000"})
+const API = axios.create({baseURL:"http://localhost:5008"})
 export const logIn = (authData) => API.post('/user/login',authData)
 export const signUp = (authData) => API.post('/user/signup',authData)
+// export this to action folder and the data user submita which is captured from post document
+
